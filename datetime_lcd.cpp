b2ef84f541adac0f9be454632994235f224b010e
kobarion <rodrigokrosa@gmail.com>
2018-11-28 22:41:04 -0200

    Changed pins for the LCD

--- datetime_lcd.cpp
+++ datetime_lcd.cpp
@@ -6,10 +6,10 @@
 //USE WIRINGPI PIN NUMBERS
 #define LCD_RS  11               //Register select pin
 #define LCD_E   10               //Enable Pin
-#define LCD_D4  0               //Data pin 4
-#define LCD_D5  5               //Data pin 5
-#define LCD_D6  2               //Data pin 6
-#define LCD_D7  3               //Data pin 7
+#define LCD_D4  25               //Data pin 4
+#define LCD_D5  24               //Data pin 5
+#define LCD_D6  23               //Data pin 6
+#define LCD_D7  22               //Data pin 7
 
 int main()
 {
